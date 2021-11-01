90a76af022b0274589af078565d9024f2ce8f3cc
Yang Q <xiaoyanq@usc.edu>
2021-10-31 18:13:07 -0700

    Initial Commit

---
+++ TextExporter/Column.cs
@@ -0,0 +1,13 @@
+namespace OpenSense.Component.TextExporter {
+    internal class Column {
+
+        /// <summary>
+        /// Exporter level column index
+        /// </summary>
+        public int Index { get; set; }
+
+        public bool Visiable { get; set; }
+
+        public string HeaderName { get; set; }
+    }
+}
